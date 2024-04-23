a57c866dc254e1fdf1d7a1df3dfc0f2dfa22d386
小手WA凉 <3100397631@qq.com>
2024-04-23 18:08:24 +0800

    提交

--- sa-club-subject/sa-club-subject-domain/src/main/java/com/xiaoshouwaliang/subject/domain/converter/SubjectBriefConverter.java
+++ sa-club-subject/sa-club-subject-domain/src/main/java/com/xiaoshouwaliang/subject/domain/converter/SubjectBriefConverter.java
@@ -1,9 +1,6 @@
 package com.xiaoshouwaliang.subject.domain.converter;
-
-import com.xiaoshouwaliang.subject.domain.entity.SubjectAnswerBO;
 import com.xiaoshouwaliang.subject.domain.entity.SubjectInfoBO;
 import com.xiaoshouwaliang.subject.infra.basic.entity.SubjectBrief;
-import com.xiaoshouwaliang.subject.infra.basic.entity.SubjectJudge;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
